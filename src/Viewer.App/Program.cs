501f9b712c7eb9369ffc7eb2acc02eaa95a7dee4
ydhappy <94909904+ydhappy@users.noreply.github.com>
2026-05-10 12:32:00 +0900

    Simplify application entry point

--- src/Viewer.App/Program.cs
+++ src/Viewer.App/Program.cs
@@ -1,5 +1,3 @@
-using System.Text;
-
 namespace Viewer.App;
 
 internal static class Program
@@ -7,7 +5,6 @@ internal static class Program
     [STAThread]
     private static void Main()
     {
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         ApplicationConfiguration.Initialize();
         Application.Run(new MainForm());
     }
